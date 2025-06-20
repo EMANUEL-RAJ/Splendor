452b1b0653a08a50885a3d21fd38f9369ea8a3d7
Emanuel Raj <emanuelraj.1999@gmail.com>
2025-06-20 20:39:20 +0530

    add assets

---
+++ data/assets.py
@@ -0,0 +1,48 @@
+from data.constants import ETHER, WATER, FIRE, AIR, EARTH
+
+elder_mages = [
+    {
+        "requirements": {ETHER: 3, WATER: 3, FIRE: 3},
+        "prestige": 3
+    },
+    {
+        "requirements": {WATER: 4, AIR: 4},
+        "prestige": 3
+    },
+    {
+        "requirements": {EARTH: 4, FIRE: 4},
+        "prestige": 3
+    },
+    {
+        "requirements": {FIRE: 4, ETHER: 4},
+        "prestige": 3
+    },
+    {
+        "requirements": {EARTH: 4, AIR: 4},
+        "prestige": 3
+    },
+    {
+        "requirements": {EARTH: 3, FIRE: 3, AIR: 3},
+        "prestige": 3
+    },
+    {
+        "requirements": {EARTH: 3, WATER: 3, AIR: 3},
+        "prestige": 3
+    },
+    {
+        "requirements": {EARTH: 3, FIRE: 3, ETHER: 3},
+        "prestige": 3
+    },
+    {
+        "requirements": {ETHER: 3, WATER: 3, AIR: 3},
+        "prestige": 3
+    },
+    {
+        "requirements": {WATER: 4, ETHER: 4},
+        "prestige": 3
+    }
+]
+
+novice_spells = [
+    
+]
---
+++ data/constants.py
@@ -0,0 +1,7 @@
+# magical elements
+FIRE = "fire"
+WATER = "water"
+EARTH = "earth"
+AIR = "air"
+ETHER = "ether"
+ARCANE = "arcane"
